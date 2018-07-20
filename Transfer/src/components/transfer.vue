2b72edb5bf89d9a8cf7aa9008429bb96b308aa1c
calamus0427 <1211994507@qq.com>
2018-07-20 16:05:39 +0800

    穿梭框组件

--- Transfer/src/components/transfer.vue
+++ Transfer/src/components/transfer.vue
@@ -1,11 +1,23 @@
 <template>
   <div class="transfer">
-    <div class="left"></div>
-    <div class="button">
-      <button>←</button>
-      <button>→</button>
+    <div class="left">
+      <ul>
+        <li @click="chooseItem(item,index)" v-for="(item,index) in left" :key="index">
+          {{item.value}}-{{item.checked}}
+        </li>
+      </ul>
     </div>
-    <div class="right"></div>
+    <div class="button">
+      <button @click="goLeft()">←</button>
+      <button @click="goRight()">→</button>
+    </div>
+    <div class="right">
+       <ul>
+        <li @click="choose(item)" v-for="(item,index) in right" :key="index">
+          {{item.value}}
+        </li>
+      </ul>
+    </div>
 
   </div>
 </template>
@@ -15,9 +27,62 @@ export default {
   name: 'transfer',
   data () {
     return {
-      data:{},
-      left:{},
-      right:{}
+      left:[{
+        'key':'1',
+        'value':'text1',
+        'disable':false
+      },{
+        'key':'2',
+        'value':'text2',
+        'disable':false
+      },{
+        'key':'3',
+        'value':'text3',
+        'disable':false
+      },{
+        'key':'4',
+        'value':'text4',
+        'disable':false
+      }],
+      right:[],
+      chooseList:[]
+    }
+  },
+  mounted(){
+    this.initData()
+  },
+  methods:{
+    initData(){
+      this.left = this.left.map((item,index,arr)=>{
+        var tmp = {
+          'key':item.key,
+          'value':item.value,
+          'disable':item.disable,
+          'checked': item.checked || false
+        }
+        return tmp
+      })
+      console.log(this.left)
+    },
+    chooseItem(item,index){
+      console.log(item.key)
+      item.checked = !item.checked
+      if(item.checked){
+        if(this.chooseList.indexOf(item)==-1){
+                this.chooseList.push(item)
+        }
+      }else{
+        this.chooseList = this.chooseList.filter((obj)=>{
+          return obj.key !== item.key
+        })
+      }
+    },
+    goLeft(){
+    },
+    goRight(){
+      console.log(this.chooseList)
+      this.right = this.right.concat(this.chooseList).reverse();
+      this.chooseList = [] ;
     }
   }
 }
